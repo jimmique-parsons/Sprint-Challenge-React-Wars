1ec490a151cbd11556ae8a9d42bb05df49a221d3
Jimmique <jimmique88@gmail.com>
2019-09-27 19:56:13 -0700

    Installed style components, styled people cards, started adding style to text, added a google font

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import People from './components/People.js'
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1% 5%;
+  justify-content: center;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -34,7 +42,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Wrapper>
         {people.map( people => <People info={people} />)}
+      </Wrapper>  
     </div>
   );
 }
--- starwars/src/components/People.js
+++ starwars/src/components/People.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const capitalizeText = (string) =>
+        string ? `${string.toUpperCase()}` : '';
+
+const Card = styled.div`
+    background: rgba(255 ,255, 255, 0.7);
+    color: rgba(0, 0, 0, 0.7);
+    padding: 2%;
+    margin: 10px;
+    border-radius: 5px;
+    text-align: left;
+    width: 14%;
+`;        
 
 export default function People({info}) {
     return (
-        <div>
+        <Card>
             <h3>Name: {info.name}</h3>
             <p>Height: {info.height}</p>
             <p>Mass: {info.mass}</p>
-        </div>
+            <p>Hair Color: {capitalizeText(info.hair_color)}</p>
+            <p>Skin Color: {capitalizeText(info.skin_color)}</p>
+            <p>Eye Color: {capitalizeText(info.eye_color)}</p>
+            <p>Birth Year: {info.birth_year}</p>
+            <p>Gender: {capitalizeText(info.gender)}</p> 
+        </Card>
     )
 }
